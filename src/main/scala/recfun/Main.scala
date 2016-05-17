9e93ae812102eb6e5edf5548f7edba0cebe44c17
William Bellamy <willbell@itv.co.uk>
2016-05-17 17:51:57 +0100

    update

---
+++ src/main/scala/recfun/Main.scala
@@ -0,0 +1,41 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+
+    //Reltionship between c and r
+    if(c < 0 || c > r) 0
+    //base case
+    else if(r == 0) 1
+
+    else {
+
+      // Number is sum of two numbers above it ie (  (c-1, r-1) + (c-1 r) )
+      pascal(c-1, r - 1) + pascal(c, r - 1)
+    }
+
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
